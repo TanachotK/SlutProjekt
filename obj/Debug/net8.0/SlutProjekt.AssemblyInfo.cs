60c0b6254be98a1d699b747e2dafa17be2bbc312
Tanachot <tanachoty0000@gmail.com>
2026-05-19 09:46:14 +0200

    ,

--- obj/Debug/net8.0/SlutProjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/SlutProjekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SlutProjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14da28ca0c26c5684ab88cc1dbb7a7820c6abd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0c13341e950c223ecb0c9589b75bc48ffe9a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("SlutProjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SlutProjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
